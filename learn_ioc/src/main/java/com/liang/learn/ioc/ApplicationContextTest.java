5b57c79d2d1254193f502ccc062c70fb5459c803
HubyLiang <hubyliang@foxmail.com>
2018-03-19 18:25:44 +0800

    iocu依赖注入

---
+++ learn_ioc/src/main/java/com/liang/learn/ioc/ApplicationContextTest.java
@@ -0,0 +1,11 @@
+package com.liang.learn.ioc;
+
+/**
+ * ApplicationContext 主要的实现类：
+ *     ClassPathXmlApplicationContext
+ *     FileSystemXmlApplicationContext
+ */
+public class ApplicationContextTest {
+
+
+}
--- learn_ioc/src/main/java/com/liang/learn/ioc/BeanFactoryTest.java
+++ learn_ioc/src/main/java/com/liang/learn/ioc/BeanFactoryTest.java
@@ -10,8 +10,13 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 
 import java.io.IOException;
 
+/**
+ * XmlBeanFactory 在Spring 3.2已被废弃，建议使用XmlBeanDefinitionReader,DefaultListableBeanFactory.
+ * Spring默认的BeanFactory是DefaultListableBeanFactory类，spring创建DefaultListableBeanFactory对象后，
+ * 会把配置信息转换成一个一个的BeanDefinition对象，并把这些BeanDefinition对象注册到DefaultListableBeanFactory对象中，
+ * 以供bean工厂创建bean实例。BeanDefinition对象存储的是单个bean的配置信息，比如依赖类、scope、是否延迟加载等等。
+ */
 public class BeanFactoryTest {
-
 
     @Test
     public void getBean() throws IOException {
@@ -22,6 +27,11 @@ public class BeanFactoryTest {
         System.out.println(resource.getURI());
 
         DefaultListableBeanFactory listableBeanFactory = new DefaultListableBeanFactory();
+
+        /**
+         * XmlBeanDefinitionReader 通过Resource装载Spring配置信息并启动Ioc容器，然后通过BeanFactory#getBean（beanName）
+         * 方法从Ioc容器中获取Bean，
+         */
         XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(listableBeanFactory);
         xmlBeanDefinitionReader.loadBeanDefinitions(resource);
 
